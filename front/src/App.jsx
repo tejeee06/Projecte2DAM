ec0647eef1fc8096b565ca64eaedf10a0655c972
tejeee06 <atejegom@gmail.com>
2025-11-06 17:23:46 +0100

    Canvis al RoutePath

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -13,7 +13,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         
         <Route path="/register" element={<Register />} />
-        
+
+        <Route path="/homePage" element={<HomePage />} />
       </Routes>
     </div>
   )
