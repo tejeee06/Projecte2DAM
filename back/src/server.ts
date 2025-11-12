d490f19ebe3c76b9d8a61af55df487d0eaef345b
tejeee06 <atejegom@gmail.com>
2025-11-13 00:10:59 +0100

    Server amb Express

---
+++ back/src/server.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import userRoutes from './routes/userRoutes';
+
+dotenv.config();
+
+const app: Express = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors()); 
+app.use(express.json()); 
+app.use('/api/users', userRoutes);
+app.get('/', (req: Request, res: Response) => {
+  res.send('Benvingut a la API de Compasity');
+});
+app.listen(PORT, () => {
+  console.log(`[server]: Servidor corriendo en http://localhost:${PORT}`);
+});
